baec1afb10609e34dfca7a6e60477c40032822b8
chinmayd-7 <155871521+chinmayd-7@users.noreply.github.com>
2025-04-15 20:03:44 +0530

    Update Bst.cpp

--- Bst.cpp
+++ Bst.cpp
@@ -1,308 +1,227 @@
-#include <iostream> #include <algorithm> 
-
-using namespace std; 
-
- 
-
-class Node { public:     int data;     Node* left; 
-
-    Node* right; 
-
- 
-
-    Node(int x) {         data = x; 
-
-        left = right = nullptr; 
-
-    } 
-
-}; 
-
- 
-
-class BST { public:     Node* root; 
-
- 
-
-    BST() {         root = nullptr; 
-
-    } 
-
- 
-
-    void create(int x) {         root = new Node(x); 
-
-        cout << "Root node created with value " << x << endl; 
-
-    } 
-
- 
-
-    void insertRecursive(int x) {         if (root == nullptr) {             create(x);         } else { 
-
-            insertRecursive(x, root); 
-
-        } 
-
-    } 
-
- 
-
-    void insertRecursive(int x, Node* node) { 
-
-        if (x < node->data) {             if (node->left == nullptr) {                 node->left = new Node(x); 
-
-                cout << "Value " << x << " inserted to the left of " << node->data << endl; 
-
-            } else {                insertRecursive(x, node->left); 
-
-           } 
-
-       } else { 
-
-           if (node->right == nullptr) {                node->right = new Node(x); 
-
-            cout << "Value " << x << " inserted to the right of " << node->data << endl; 
-
-    } else { 
-
-        insertRecursive(x, node->right); 
-
-   } 
-
-} }
-
-   void insertIterative(int x) { 
-
-       Node* newNode = new Node(x); 
-
-       if (root == nullptr) {            create(x); 
-
-            return; 
-
-        } 
-
- 
-
-        Node* current = root; 
-
-        Node* parent = nullptr; 
-
- 
-
-        while (current != nullptr) {             parent = current;             if (x < current->data) {                 current = current->left; 
-
-            } else { 
-
-                current = current->right; 
-
-            } 
-
-        } 
-
- 
-
-        if (x < parent->data) {             parent->left = newNode; 
-
-            cout << "Value " << x << " inserted to the left of " << parent->data << endl; 
-
-        } else { 
-
-            parent->right = newNode; 
-
-            cout << "Value " << x << " inserted to the right of " << parent->data << endl; 
-
-        } 
-
-    } 
-
- 
-
-    bool searchRecursive(int x) { 
-
-        cout << "Searching path for value " << x << ":" << endl;         return searchRecursive(x, root); 
-
-    } 
-
- 
-
-    bool searchRecursive(int x, Node* node) {         if (node == nullptr) { 
-
-            cout << "Element not found." << endl;             return false; 
-
-        } 
-
-        cout << "[" << node->data << "] -> ";        if (x == node->data) { 
-
-           cout << "Element Found!" << endl; 
-
-           return true; 
-
-       } 
-
-       if (x < node->data) { 
-
-        cout << "Smaller, going to left subtree." << endl; 
-
-    return searchRecursive(x, node->left); } else { 
-
-   cout << "Greater, going to right subtree." << endl; 
-
-    return searchRecursive(x, node->right); 
-
-} }
-
-   bool searchIterative(int x) {        Node* current = root;        cout << "Searching path for value " << x << ":" << endl; 
-
- 
-
-        while (current != nullptr) {             cout << "[" << current->data << "] -> ";             if (x == current->data) {                 cout << "Element Found!" << endl; 
-
-                return true; 
-
-            } 
-
-            if (x < current->data) { 
-
-                cout << "Smaller, going to left subtree." << endl; 
-
-                current = current->left; 
-
-            } else { 
-
-                cout << "Greater, going to right subtree." << endl; 
-
-                current = current->right; 
-
-            } 
-
-        } 
-
-        cout << "Element not found." << endl; 
-
-        return false; 
-
-    } 
-
- 
-
-    int findHeight(Node* node) {         if (node == nullptr) { 
-
-            return 0; 
-
-        } 
-
-        int leftHeight = findHeight(node->left);         int rightHeight = findHeight(node->right); 
-
-        return max(leftHeight, rightHeight) + 1; 
-
-    } 
-
- 
-
-    int findMinValue(Node* node) {         if (node == nullptr) {             cout << "Tree is empty." << endl; 
-
-            return -1; 
-
-        } 
-
-        Node* current = node;         while (current->left != nullptr) { 
-
-           current = current->left; 
-
-       } 
-
-       return current->data; 
-
-   } 
-
-void mirrorTree(Node* node) { 
-
-if (node == nullptr) {     return; 
-
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node *left;
+		Node *right;
+		Node(int x){
+			data=x;
+			left=NULL;
+			right=NULL;
+		}
+};
+
+class bst{
+	public:
+		Node* root;
+		bst(){
+			root=NULL;
+		}
+		
+		Node* create(int x){
+			root=new Node(x);
+			return root;
+		}
+		void insert(int x,Node *root){
+			Node *temp=root;
+			if(x<=temp->data){
+				if(temp->left==NULL){
+					temp->left=new Node(x);
+					cout<<x<<" Inserted at left"<<endl;
+					return;
+				}
+				else if(temp->left!=NULL){
+					cout<<"Going to left subtree"<<endl;
+					insert(x,temp->left);
+				}
+			}
+			else if(x>temp->data){
+				if(temp->right==NULL){
+					temp->right=new Node(x);
+					cout<<x<<" Inserted at right"<<endl;
+					return;
+				}
+				else if(temp->right!=NULL){
+					cout<<"Going to right subtree"<<endl;
+					insert(x,temp->right);
+				}
+			}
+		}
+		
+		void insert1(int x,Node *root){
+			Node *temp=root;
+			while(temp!=NULL){
+				if(x<=temp->data){
+					if(temp->left==NULL){
+						temp->left=new Node(x);
+						cout<<x<<" Inserted at left"<<endl;
+						return;
+					}
+					cout<<"Going to left subtree"<<endl;
+					temp=temp->left;
+				}
+				else{
+					if(temp->right==NULL){
+						temp->right=new Node(x);
+						cout<<x<<" Inserted at right"<<endl;
+						return;
+					}
+					cout<<"Going to right subtree"<<endl;
+					temp=temp->right;
+				}
+			}
+			//cout<<x<<" Inserted"<<endl;
+			temp=new Node(x);
+		}
+		
+		void search(int x,Node* root){
+			Node *temp=root;
+			if(temp->data==x){
+				cout<<x<<" found at root\n"<<endl;
+				return;
+			}
+			else if(x<=temp->data){
+				if(temp->left==NULL){
+					cout<<x<<" does not exist in BST\n"<<endl;
+					return;
+				}
+				else if(temp->left!=NULL){
+					cout<<"Going to left subtree"<<endl;
+					search(x,temp->left);
+				}
+			}
+			else if(x>temp->data){
+				if(temp->right==NULL){
+					cout<<x<<" does not exist in BST\n"<<endl;
+					return;
+				}
+				else if(temp->right!=NULL){
+					cout<<"Going to right subtree"<<endl;
+					search(x,temp->right);
+				}
+			}
+		}
+		
+		void search1(int x,Node *root){
+			Node *temp=root;
+			while(temp!=NULL){
+				if(temp->data==x){
+					cout<<x<<" found at root\n"<<endl;
+					return;
+				}
+				else if(x<=temp->data){
+					cout<<"Going to left subtree"<<endl;
+					temp=temp->left;
+				}
+				else if(x>temp->data){
+					cout<<"Going to right subtree"<<endl;
+					temp=temp->right;
+				}
+			}
+			cout<<x<<" does not exist in BST\n"<<endl;
+			
+		}
+		
+		//just for debugging swaping
+		void search2(int x,Node *root){
+			Node *temp=root;
+			while(temp!=NULL){
+				if(temp->data==x){
+					cout<<x<<" found at root\n"<<endl;
+					return;
+				}
+				else if(x>=temp->data){
+					cout<<"Going to left subtree"<<endl;
+					temp=temp->left;
+				}
+				else if(x<temp->data){
+					cout<<"Going to right subtree"<<endl;
+					temp=temp->right;
+				}
+			}
+			cout<<x<<" does not exist in BST\n"<<endl;
+			
+		}
+		
+		void swap(Node *root){
+			//swap left ptr with right in all nodes
+			if(root==NULL)
+				return;
+			Node *swap_temp=root->left;
+			root->left=root->right;
+			root->right=swap_temp;
+			
+			swap(root->left);
+			swap(root->right);
+		}
+};
+int main(){
+	bst b;
+	Node *root=b.create(20);
+	cout<<"Root is 20"<<endl;
+	cout<<endl<<"insert(30)"<<endl;
+	b.insert(30,root);
+	cout<<endl<<"insert(25)"<<endl;
+	b.insert(25,root);
+	cout<<endl<<"insert(35)"<<endl;
+	b.insert(35,root);
+	cout<<endl<<"insert(12)"<<endl;
+	b.insert(12,root);
+	cout<<endl<<"insert(15)"<<endl;
+	b.insert(15,root);
+	cout<<endl<<"insert(50)"<<endl;
+	b.insert(50,root);
+	cout<<endl<<"insert(45)"<<endl;
+	b.insert(45,root);
+	cout<<endl<<"insert(5)"<<endl;
+	b.insert(5,root);
+	cout<<endl<<"insert(20)"<<endl;
+	b.insert(20,root);
+	
+	cout<<endl<<"************************************";
+	cout<<endl<<"Inserting using loops"<<endl;
+	bst b1;
+	Node *root1=b1.create(20);
+	cout<<"Root is 20"<<endl;
+	cout<<endl<<"insert(30)"<<endl;
+	b1.insert1(30,root1);
+	cout<<endl<<"insert(25)"<<endl;
+	b1.insert1(25,root1);
+	cout<<endl<<"insert(35)"<<endl;
+	b1.insert1(35,root1);
+	cout<<endl<<"insert(12)"<<endl;
+	b1.insert1(12,root1);
+	cout<<endl<<"insert(15)"<<endl;
+	b1.insert1(15,root1);
+	cout<<endl<<"insert(50)"<<endl;
+	b1.insert1(50,root1);
+	cout<<endl<<"insert(45)"<<endl;
+	b1.insert1(45,root1);
+	cout<<endl<<"insert(5)"<<endl;
+	b1.insert1(5,root1);
+	
+	cout<<endl<<"************************************";
+	cout<<endl<<"searching using recursion"<<endl;
+	b.search(15,root);
+	b.search(50,root);
+	b.search(1,root);
+	
+	cout<<endl<<"************************************";
+	cout<<endl<<"searching using loops"<<endl;
+	b1.search1(15,root1);
+	b1.search1(50,root1);
+	b1.search1(1,root1);
+	
+	cout<<endl<<"************************************";
+	b1.swap(root1);
+	cout<<endl<<" searching after swaping"<<endl;
+	b1.search2(15,root1);
+	b1.search2(50,root1);
+	b1.search2(1,root1);
+	
+	return 0;
 }
 
-swap(node->left, node->right); mirrorTree(node->left); 
 
-   mirrorTree(node->right); 
-
-   } 
-
-   void mirror() {        mirrorTree(root); 
-
-        cout << "Tree structure mirrored successfully." << endl; 
-
-    } 
-
-}; 
-
- 
-
-int main() {     BST tree; 
-
-    int ch, value; 
-
- 
-
-    do { 
-
-        cout << "-------------Binary Search Tree-------------" << endl;         cout << "1. Insert a Node (Recursion)" << endl;         cout << "2. Insert a Node (Loop)" << endl;         cout << "3. Search a Node (Recursion)" << endl;         cout << "4. Search a Node (Loop)" << endl; 
-
-        cout << "5. Find Number of Nodes in Longest Path (Tree Height)" << endl;         cout << "6. Find Minimum Data Value in the Tree" << endl;         cout << "7. Mirror the Tree Structure" << endl; 
-
-        cout << "8. Exit" << endl;         cout << "Enter your choice: "; 
-
-        cin >> ch; 
-
- 
-
-        switch (ch) {             case 1: 
-
-                cout << "Enter value to insert: ";                 cin >> value;                 tree.insertRecursive(value); 
-
-                break;             case 2: 
-
-                cout << "Enter value to insert: "; 
-
-                cin >> value;                 tree.insertIterative(value); 
-
-                break;             case 3: 
-
-                cout << "Enter value to search: ";                 cin >> value; 
-
-               if (!tree.searchRecursive(value)) { 
-
-                   cout << value << " not found in the tree." << endl; 
-
-               }                break;            case 4: 
-
-            cout << "Enter value to search: ";         cin >> value; 
-
-        if (!tree.searchIterative(value)) { 
-
-           cout << value << " not found in the tree." << endl; 
-
-        } 
-
-        break; 
-
-       case 5: 
-
-                cout << "Longest path from root (Tree Height): " << tree.findHeight(tree.root) << endl;                 break;             case 6: 
-
-                cout << "Minimum data value in the tree: " << tree.findMinValue(tree.root) << endl;                 break;             case 7:                 tree.mirror();                 break;             case 8:                 cout << "Exiting..." << endl; 
-
-                break;             default: 
-
-                cout << "Invalid choice. Try again." << endl; 
-
-        } 
-
-    } while (ch != 8); 
-
- 
-
-    return 0; 
-
-} 
 
