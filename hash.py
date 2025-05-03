c7a8b18634117aa7466a5389d47c558184a5fcee
chinmayd-7 <155871521+chinmayd-7@users.noreply.github.com>
2025-05-03 07:27:11 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -16,33 +16,43 @@ class double_hash:
         if self.hashtable[index] is None:
             self.hashtable[index] = [data[0], data[1]]
             print("Element inserted at index", index)
-            print(self.hashtable)
         else:
             self.double_hashing(data)
 
-    def double_hashing(self, data):
-        i = 0
-        index = (self.h1(data) + i * self.h2(data)) % self.size
-        while self.hashtable[index] is not None:
-            i += 1
-            self.comparisons += 1
-            index = (self.h1(data) + i * self.h2(data)) % self.size
-        self.hashtable[index] = [data[0], data[1]]
-        print("Element inserted at index", index)
         print(self.hashtable)
 
-    def search(self, data):
-        i = 0
-        index = (self.h1(data) + i * self.h2(data)) % self.size
-        while self.hashtable[index] != data:
-            i += 1
+    def double_hashing(self, data):
+        # Using for loop for probing
+        for i in range(self.size):
             index = (self.h1(data) + i * self.h2(data)) % self.size
-            if i == self.size - 1:
-                break
-        if i == self.size - 1:
-            print("Element not present")
-        else:
+            if self.hashtable[index] is None:
+                self.hashtable[index] = [data[0], data[1]]
+                print("Element inserted at index", index)
+                print(self.hashtable)
+                return
+        print("Hash table is full, cannot insert element.")
+
+    def search(self, data):
+        index = self.h1(data)
+        if self.hashtable[index] is not None and self.hashtable[index][0] == data[0]:
             print("Element found at index", index)
+            return self.hashtable[index]
+        else:
+            return self.double_hash_search(data)
+
+    def double_hash_search(self, data):
+        # Using for loop for probing
+        for i in range(self.size):
+            new_index = (self.h1(data) + i * self.h2(data)) % self.size
+            if self.hashtable[new_index] is None:
+                print("Element not found.")
+                return None
+            elif self.hashtable[new_index][0] == data[0]:
+                print("Element found at index", new_index)
+                return self.hashtable[new_index]
+        print("Element not found.")
+        return None
+
 
 class quadratic_hash:
     def __init__(self, size):
@@ -55,26 +65,42 @@ class quadratic_hash:
 
     def insert(self, data):
         index = self.h1(data)
-        i = 0
-        while self.hashtable[index] is not None:
-            i += 1
-            index = (self.h1(data) + i * i) % self.size  # Quadratic probing
-        self.hashtable[index] = [data[0], data[1]]
-        print("Element inserted at index", index)
+        if self.hashtable[index] is None:
+            self.hashtable[index] = [data[0], data[1]]
+            print("Element inserted at index", index)
+        else:
+            self.quadratic_probing(data)
+
         print(self.hashtable)
 
+    def quadratic_probing(self, data):
+        for i in range(1, self.size):
+            new_index = (self.h1(data) + i * i) % self.size
+            if self.hashtable[new_index] is None:
+                self.hashtable[new_index] = [data[0], data[1]]
+                print("Element inserted at index", new_index)
+                return
+        print("Hash table is full, cannot insert element.")
+        
     def search(self, data):
         index = self.h1(data)
-        i = 0
-        while self.hashtable[index] is not None:
-            if self.hashtable[index] == data:
-                print("Element found at index", index)
-                return
-            i += 1
-            index = (self.h1(data) + i * i) % self.size  # Quadratic probing
-            if i == self.size - 1:
-                break
-        print("Element not present")
+        if self.hashtable[index] is not None and self.hashtable[index][0] == data[0]:
+            print("Element found at index", index)
+            return self.hashtable[index]
+        else:
+            return self.quadratic_search(data)
+
+    def quadratic_search(self, data):
+        for i in range(1, self.size):
+            new_index = (self.h1(data) + i * i) % self.size
+            if self.hashtable[new_index] is None:
+                print("Element not found.")
+                return None
+            elif self.hashtable[new_index][0] == data[0]:
+                print("Element found at index", new_index)
+                return self.hashtable[new_index]
+        print("Element not found.")
+        return None
 
 # Main Menu
 while True:
